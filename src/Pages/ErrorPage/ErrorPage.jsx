9487114d72e27d1180410572fb7789370c9e4f4c
toma570 <itextoma@gmail.com>
2023-06-10 18:16:24 +0600

    add error

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,36 @@
 
-
+import Lottie from "react-lottie";
+import errorPage from '../../../public/error-404.json'
+import Container from "../../Components/Container/Container";
+import { Link } from "react-router-dom";
 const ErrorPage = () => {
+    const defaultOptions = {
+		loop: true,
+		autoplay: true,
+		animationData: errorPage,
+		rendererSettings: {
+			preserveAspectRatio: "xMidYMid slice",
+		},
+	};
     return (
-        <div>
-            <p>error</p>            
-        </div>
-    );
+		<div>
+			<Container>
+				<Lottie
+					options={defaultOptions}
+					height={"80vh"}
+					width={400}
+				/>
+
+				<div className='text-center'>
+					<Link to={"/"}>
+						<button className='py-3 px-10 bg-[#1A2E35] hover:bg-[#272145] rounded-md text-white'>
+							Home Page
+						</button>
+					</Link>
+				</div>
+			</Container>
+		</div>
+	);
 };
 
 export default ErrorPage;
